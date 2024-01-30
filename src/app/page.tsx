87348395af99c832f023a1dceff09201e9f1ff05
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2024-01-30 13:54:09 +0100

    Add textbox and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return;
+  return (
+    <div className="flex items-center justify-center my-8">
+      <input></input>
+      <form>
+        <label>
+          <input type="text" name="name" className="bg-slate-300" placeholder="Send Message" />
+        </label>
+        <button className="bg-slate-200 hover:bg-slate-300 rounded-md p-2">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 }
