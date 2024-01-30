970b1fcbcd61ea5ee1d6d604255757dd27497084
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2024-01-30 15:48:57 +0100

    Added messages
    Co-Authored-By: Johannes <65119813+Johannes29@users.noreply.github.com>
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>

---
+++ src/app/messageSystem.tsx
@@ -0,0 +1,34 @@
+import Message from "./messages";
+
+export default function MessageSystem() {
+    return (
+        <div>
+            <div className="flex justify-center my-8">
+                <form>
+                    <label>
+                        <input type="text" name="name" className="bg-slate-300" placeholder="Send Message" />
+                    </label>
+                    <button className="bg-slate-200 hover:bg-slate-300 px-2">
+                        Send
+                    </button>
+                </form>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+                <Message name="John Doe" message="Hello, how are you today?yfyiktfytfyujtfjytfjytfrftgyhujikoijuhygfcvgbhjiuhyhgtvbhjn" />
+                <Message name="John Doe" message="Hello, how are you today?" />
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
+import MessageSystem from "./messageSystem";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center my-8">
-      <input></input>
-      <form>
-        <label>
-          <input type="text" name="name" className="bg-slate-300" placeholder="Send Message" />
-        </label>
-        <button className="bg-slate-200 hover:bg-slate-300 rounded-md p-2">
-          Send
-        </button>
-      </form>
+    <div>
+      <body>
+        <MessageSystem />
+      </body>
     </div>
   );
 }
