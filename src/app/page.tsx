59a74b91603909694c985c677f3a651b11990711
Johannes <65119813+Johannes29@users.noreply.github.com>
2024-01-30 16:01:05 +0100

    Add chat proof of concept
    Co-Authored-By: Tore Forsberg <142976914+Tore-Forsberg@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,62 @@
+"use client";
+// TODO fix
+// import styles from "./page.module.css";
+import { io } from "socket.io-client";
+import { useState } from "react";
+import ChatPage from "@/components/page";
+
 export default function Home() {
-  return;
+  const [showChat, setShowChat] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [showSpinner, setShowSpinner] = useState(false);
+  const [roomId, setroomId] = useState("");
+
+  var socket: any;
+  socket = io("http://localhost:3001");
+
+  const handleJoin = () => {
+    if (userName !== "" && roomId !== "") {
+      console.log(userName, "userName", roomId, "roomId");
+      socket.emit("join_room", roomId);
+      setShowSpinner(true);
+// You can remove this setTimeout and add your own logic
+      setTimeout(() => {
+        setShowChat(true);
+        setShowSpinner(false);
+      }, 4000);
+    } else {
+      alert("Please fill in Username and Room Id");
+    }
+  };
+
+  return (
+    <div>
+      <div
+        style={{ display: showChat ? "none" : "" }}
+      >
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setUserName(e.target.value)}
+          disabled={showSpinner}
+        />
+        <input
+          type="text"
+          placeholder="room id"
+          onChange={(e) => setroomId(e.target.value)}
+          disabled={showSpinner}
+        />
+        <button onClick={() => handleJoin()}>
+          {!showSpinner ? (
+            "Join"
+          ) : (
+            <div></div>
+          )}
+        </button>
+      </div>
+      <div style={{ display: !showChat ? "none" : "" }}>
+        <ChatPage socket={socket} roomId={roomId} username={userName} />
+      </div>
+    </div>
+  );
 }
